031a52ea90af75b96a832cb730e6a03f61fef818
Ethan Hearnsberger <411ethan114@gmail.com>
2022-09-15 11:49:40 -0700

    initial commit

---
+++ assets/javascript/script.js
@@ -0,0 +1,10 @@
+//set date
+var d = new Date();
+
+var month = d.getMonth()+1;
+var day = d.getDate();
+
+var output = d.getFullYear() + '/' +
+    (month<10 ? '0' : '') + month + '/' +
+    (day<10 ? '0' : '') + day;
+    
